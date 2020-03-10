40a23d29b3e6db0fa6a371f0f1ee8ad60696c870
Wojciech Stachowiak <stachowiakw@gmail.com>
2020-03-10 23:41:44 +0100

    Lecture 125

---
+++ Udemy_RealmRush/Assets/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1,10)]
+    private int gridSize = 10;
+
+        void Update()
+        {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
+        }
+}
