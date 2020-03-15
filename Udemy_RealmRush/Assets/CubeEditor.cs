e85c26d7af5cec5dcf7367db0a6a48364985d3ad
Wojciech Stachowiak <stachowiakw@gmail.com>
2020-03-15 02:37:31 +0100

    Lecture 128

--- Udemy_RealmRush/Assets/CubeEditor.cs
+++ Udemy_RealmRush/Assets/CubeEditor.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EditorSnap : MonoBehaviour
+
+[ExecuteInEditMode]
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1,10)]
     private int gridSize = 10;
 
-        void Update()
-        {
+    TextMesh textMesh;
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+
         Vector3 snapPosition;
         snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
-        }
+        string labelText = snapPosition.x / gridSize + "," + snapPosition.z / gridSize;
+
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
 }
---
+++ Udemy_RealmRush/Assets/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    List<Waypoint2> Path;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+        print("But I can still do other stuff :)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   IEnumerator FollowPath()
+    {
+        print("Starting patrol...");
+        foreach (Waypoint2 waypoint in Path)
+        {
+            print("Visiting: "+ waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+        print("Ending patrol.");
+    }
+}
