84644be1c3775d4112275eb8dfbe68a623766a2c
Wojciech Stachowiak <stachowiakw@gmail.com>
2020-03-23 01:04:08 +0100

    Lecture 141

---
+++ Udemy_RealmRush/Assets/Tower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        lookAtEnemy();  
+    }
+
+    void lookAtEnemy()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
